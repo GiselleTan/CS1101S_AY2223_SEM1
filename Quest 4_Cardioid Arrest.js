6d0769cef26ba782adad9c55c48544a893cfac20
Giselle Tan <giselletan1008@gmail.com>
2022-11-20 19:15:01 +0800

    Create Quest 4_Cardioid Arrest.js

---
+++ Quest 4_Cardioid Arrest.js
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////
+//QUEST 4: CARDIOID ARREST (XP: 100 / 500)
+
+//Q4, TASK 1
+const connect_numbers =
+    n => draw_connected_full_view(n)(unit_circle);
+
+connect_numbers(5); // returns a Drawing of a pentagon
+
+/*
+function unit_circle(t) {
+    return make_point(math_cos(2 * math_PI * t),
+                      math_sin(2 * math_PI * t));
+}
+*/
+
+
+
+
+//LEFT REST OF THE QUESTIONS BLANK
