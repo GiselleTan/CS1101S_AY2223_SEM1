f411be8e48ae4476b0067f4fa06d84d8ac92798e
Giselle Tan <giselletan1008@gmail.com>
2022-11-25 17:24:06 +0800

    Create Path 1_Elements of Programming.js

---
+++ Path 1_Elements of Programming.js
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////
+//PATH 1: ELEMENTS OF PROGRAMMING (XP: 25 / 0)
+
+//P1, TASK 1
+2 + 3;
+
+
+
+//P1, TASK 2
+const two = 3;
+const seven = 4;
+const three = 7;
+(two * three) + seven;
+25;
+
+
+
+//P1, TASK 3
+const foo = 10;
+const bar = 10 + 20;
+const foobar = foo + bar;
+
+
+
+//P1, TASK 4
+function bar(){
+    return 20;
+}
+
+
+
+//P1, TASK 5
+function square(x) {
+    return x * x;
+}
+
+function square_of_difference(a,b){
+    return square(a-b);
+}
+
+square_of_difference(2,5);
+square_of_difference(12,10);
