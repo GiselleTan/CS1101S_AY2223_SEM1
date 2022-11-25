e68f1a7e09b59d82a1c18bc0ed5b6a388c5abfac
Giselle Tan <giselletan1008@gmail.com>
2022-11-25 17:57:08 +0800

    Create Path 9_Programming language processing.js

---
+++ Path 9_Programming language processing.js
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////
+//PATH 9: PROGRAMMING LANGUAGE PROCESSING (XP: 85 / 60)
+
+//P9, TASK 1
+//This diagram depicts an interpreter that can interpret programs written in Swift
+
+
+
+//P9, TASK 2
+//This diagram depicts a compiler that produces programs written in C.
+
+
+
+
+//P9, TASK 3
+//The diagram is correct
+
+
+
+
+//P9, TASK 4
+//The diagram is correct
+
+
+
+
+//P9, TASK 5
+//The diagram is incorrect because the to-language doesn't match
+
+
+
+
+//P9, TASK 6
+//The diagram is correct
